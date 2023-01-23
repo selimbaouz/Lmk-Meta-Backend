c5a877d2e1ee4a12a07c39f70e3d8244ba8648e1
sayrodev <sayrodev@gmail.com>
2023-01-23 01:49:14 +0100

    add new error

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -21,6 +21,8 @@ export const createContact = (req, res) => {
                 res.status(400).send(error.response.data);
             } else if(error.response.data.code === 11) {
                 res.status(400).send(error.response.data);
+            } else if(error.response.data.code === 19) {
+                res.status(400).send(error.response.data);
             } else {
                 res.status(500).send(error.response.data);
             }
